1be53a33aedd573aaf60a7b4cdd73652c0767832
JoyelTheDev <222600183+JoyelTheDev@users.noreply.github.com>
2025-12-09 07:49:17 +0530

    Fixed MixinScreen.java

--- src/main/java/org/bleachhack/mixin/MixinScreen.java
+++ src/main/java/org/bleachhack/mixin/MixinScreen.java
@@ -8,56 +8,29 @@
  */
 package org.bleachhack.mixin;
 
-import java.util.List;
-
-import net.minecraft.client.gui.tooltip.TooltipPositioner;
 import org.bleachhack.BleachHack;
 import org.bleachhack.event.events.EventRenderScreenBackground;
-import org.bleachhack.event.events.EventRenderTooltip;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.util.math.MatrixStack;
 
 @Mixin(Screen.class)
 public class MixinScreen {
 
-	@Unique private int lastMX;
-	@Unique private int lastMY;
-
-	@Unique private boolean skipTooltip;
-
-	@Shadow private void renderTooltipFromComponents(MatrixStack matrices, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner) {}
+	@Unique
+	private int lastMX;
+	@Unique
+	private int lastMY;
 
 	@Inject(method = "render", at = @At("HEAD"))
 	private void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callback) {
 		lastMX = mouseX;
 		lastMY = mouseY;
-	}
-
-	@Inject(method = "renderTooltipFromComponents", at = @At("HEAD"), cancellable = true)
-	private void renderTooltipFromComponents(MatrixStack matrices, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, CallbackInfo callback) {
-		if (skipTooltip) {
-			skipTooltip = false;
-			return;
-		}
-
-		EventRenderTooltip event = new EventRenderTooltip((Screen) (Object) this, matrices, components, x, y, lastMX, lastMY);
-		BleachHack.eventBus.post(event);
-
-		callback.cancel();
-		if (event.isCancelled()) {
-			return;
-		}
-
-		skipTooltip = true;
-		renderTooltipFromComponents(event.getMatrix(), event.getComponents(), event.getX(), event.getY(), positioner);
 	}
 
 	@Inject(method = "renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V", at = @At("HEAD"), cancellable = true)
